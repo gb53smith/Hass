463f73661dc4fc6b5e6c0f4a2b90d81ec8a7829e
Graham Smith <gb53smith@gmail.com>
2018-03-18 11:32:16 -0700

    pylint clean up

--- getrain.py
+++ getrain.py
@@ -1,23 +1,21 @@
-#!/usr/bin/python3.4
+'''
+Reads weather report from gc.ca website in xml format.  Extracts
+and returns yesterday's rainfall.
+'''
+import xml.etree.ElementTree as ET
+from urllib import request
 
-import urllib.request
-import urllib.parse 
-import re 
-import xml.etree.ElementTree as ET 
-from urllib import request 
-
-#Now I can make the API call.  
-id_request = urllib.request.urlopen('http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/BC/s0000141_e.xml')
+# Now I can make the API call.
+id_request = request.urlopen\
+    ('http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/BC/s0000141_e.xml')
 
 #Let's now read this baby in XML format!
 id_pubmed = id_request.read()
 root = ET.fromstring(id_pubmed)
 
 # Find the data I want
-#elements are numbered with integers.  Uncomment to see order of elements
+# elements are numbered with integers.  Uncomment to see order of elements
 #for child in root:
 	#print(child.tag,child.attrib)
 #This is yesterday's rainfall in mm.
 print(root[8][2].text)
-
-
