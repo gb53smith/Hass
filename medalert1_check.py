779cab65c2299fbdadd81796974b228c429cd2db
Graham Smith <gb53smith@gmail.com>
2018-05-09 15:35:04 -0700

    Add sensor and alarm for Medalert1.

---
+++ medalert1_check.py
@@ -0,0 +1,13 @@
+'''
+Checks that medalert1 Home Assistant is running.
+'''
+from urllib.request import urlopen
+from urllib.error import URLError
+try:
+    HTML = urlopen("https://medalert1.duckdns.org:8123")
+    if "Home Assistant" in str(HTML.read()):
+        print("Up")
+    else:
+        print("Down")
+except URLError:
+    print("URLError")
