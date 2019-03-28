e0dddc2269c8d16b4394518bcc6511f838ddf3d3
Graham Smith <gb53smith@gmail.com>
2019-03-27 22:35:32 -0700

    Change medalert1 check method to use curl. Update ui-lovelace.yaml

--- medalert1_check.py
+++ medalert1_check.py
@@ -6,19 +6,19 @@ from urllib.request import urlopen
 from urllib.error import URLError
 count = 0
 try:
-    HTML = urlopen("https://medalert1.duckdns.org:8123")
+    HTML = urlopen("https://medalert1.duckdns.org:8123", timeout=3)
     if "Home Assistant" in str(HTML.read()):
         count += 1
 except URLError:
     pass
 try:
-    HTML = urlopen("https://medalert1.duckdns.org:8123")
+    HTML = urlopen("https://medalert1.duckdns.org:8123", timeout=3)
     if "Home Assistant" in str(HTML.read()):
         count += 1
 except URLError:
     pass
 try:
-    HTML = urlopen("https://medalert1.duckdns.org:8123")
+    HTML = urlopen("https://medalert1.duckdns.org:8123", timeout=3)
     if "Home Assistant" in str(HTML.read()):
         count += 1
 except URLError:
