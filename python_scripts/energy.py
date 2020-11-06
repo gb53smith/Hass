154622d66685064da3e99b033cab3612b6bfb3f7
Graham Smith <gb53smith@gmail.com>
2020-11-05 22:21:19 -0800

    Improved energy calculation.

--- python_scripts/energy.py
+++ python_scripts/energy.py
@@ -1,13 +1,16 @@
 
 '''
-Date:  April 16, 2020
+Date:  November 5, 2020
 
-Versions used: HA108.0, HassOS and Raspberry PI 3 B
+Versions used: HA116.1, HassOS and Raspberry PI 3 B
 
 Description:
-This Python Script converts histograph type power to energy accumulation over one hour
-It needs three input_number entities to be passed as data values:  power, last_power and hourly_energy
-input_number.energy_accum is used to accumulate the energy calculation over an hoour period.
+This Python Script converts histogram type power to energy accumulation.
+It needs two input_number entities to be passed as data values:  power, last_power.
+input_number.energy_accum is used to accumulate the energy calculation.
+The hourly energy calculation is done in a separate script called energy_hour.py
+The hourly energy script waits for this script to complete, preventing errors to due
+to power changes that occur on the hour boundary.
 input_number entities restore the last known value on HA restart.  Sensors do not restore.
 
 Instructions:
@@ -16,7 +19,7 @@ Instructions:
    1.2 Add this line to configuration.yaml
        python_scripts:    
         
-2.  Add these input_number entity to configuration.yaml
+2.  Add these input_number entities to configuration.yaml
     input_number used because their values are restored after HA restarts.
     
     furnace_power:
@@ -36,39 +39,33 @@ Instructions:
       min: 0
       max: 1000
       step: 0.1 
-      
-    hourly_energy:
-      min: 0
-      max: 100
-      step: 0.1
-      mode: box 
         
 3.  Add this automation to automations.yaml
     * items are your choosing but must match your sensor names above
     input_number.furnace_power is the input power sensor in this example
 
-  - alias: Hourly Energy
-    initial_state: True
-    trigger:
-      - platform: state
-        entity_id: input_number.furnace_power*
-      - platform: time_pattern
-        hours: '/1'      
-    action:
-      service: python_script.energy
+- alias: Energy Integration
+  initial_state: True
+  trigger:
+    - platform: state
+      entity_id: input_number.furnace_power
+  action:
+    service: script.turn_on
+    entity_id: script.energy
+    
+4. Add this script to scripts.yaml
+
+energy:
+  alias: Energy Integrator
+  sequence: 
+    - service: python_script.energy
       data:
-        power: furnace_power*
-        last_power: last_power*
-        energy_accum: energy_accum*
-        hourly_energy: hourly_energy*
+        power: furnace_power
+        last_power: last_power
+        energy_accum: energy_accum
 
-4. Use the history_graph lovelace card to display the result.
+5. Use the history_graph lovelace card to display the result.
 
-5. Use the Statistics Sensor to average hourly energy over longer periods.  (Not tried yet)
-
-6. Save database size by not recording the last power and energy accumulator values.  
-   Only the last value needed by the algorithm to calculate the hourly energy.
-   The last value of all sensors is recorded in the database.  
 
 '''
 
@@ -78,7 +75,6 @@ WINDOW_SIZE = 60*60
 power = data.get('power', '0')
 last_power = data.get('last_power', '0')
 energy_accum = data.get('energy_accum', '0')
-hourly_energy = data.get('hourly_energy', '0')
 
 if power != '0':
 	input_number_power = "input_number." + power
@@ -95,16 +91,6 @@ if energy_accum != '0':
 else:
 	logger.error("Energy script missing energy_accum data.")
 
-if hourly_energy != '0':
-	input_number_hourly_energy = "input_number." + hourly_energy
-else:
-	logger.error("Energy script missing hourly_energy data.")
-    
-time = datetime.datetime.now()
-time_sec = time.second
-time_min = time.minute
-timestamp = time.timestamp()
-
 # Get power unit and use to set the units for the other sensors used.
 power_attr = hass.states.get(input_number_power).attributes
 power_unit = power_attr['unit_of_measurement']
@@ -112,59 +98,35 @@ energy_unit = power_unit + 'h'
     
 energy_accum_state = hass.states.get(input_number_energy_acum)
 energy_accum = float(energy_accum_state.state)
+   
+power_state = hass.states.get(input_number_power)
+power = float(power_state.state)
 
-#Clear energy accumulation at start of interval.
-#Transfer sum of energy from previous period to the hourly log
-if time_sec == 0 and time_min == 0:
-    #logger.warning("Start of Window")
-    last_power_state = hass.states.get(input_number_last_power)
-    last_power = float(last_power_state.state)
+#Need to get last_power_state since it is reset at start of window
+last_power_state = hass.states.get(input_number_last_power)
+last_power = float(last_power_state.state)
+#No new energy if last power was zero.
+if last_power > 0:
     ##logger.warning("Last Power = {}".format(last_power))
     last_power_change = last_power_state.last_changed.timestamp()
-    delta_time = timestamp - last_power_change
-    ##logger.warning("Time since last power change = {}".format(delta_time))    
-    #Add remnant from last window
-    if last_power > 0:
-        remnant = last_power * delta_time / WINDOW_SIZE
-        energy_accum = energy_accum + remnant
-        #refresh last_power to change the timestamp to beginning of window period
-        hass.states.set(input_number_last_power, (last_power + 0.1), {"unit_of_measurement": power_unit})
-        hass.states.set(input_number_last_power, last_power, {"unit_of_measurement": power_unit})
-    #Update hourly energy
-    hass.states.set(input_number_hourly_energy, round(energy_accum, 1), {"unit_of_measurement": energy_unit})        
-    # Clear energy accumulation for the start of the next window
-    hass.states.set(input_number_energy_acum, 0, {"unit_of_measurement": energy_unit})
-    #logger.warning("Last Window energy_accum = {}".format(energy_accum))    
-else:
-    #logger.warning("Within Window")
-    power_state = hass.states.get(input_number_power)
-    power = float(power_state.state)
- 
-    #Need to get last_power_state since it is reset at start of window
-    last_power_state = hass.states.get(input_number_last_power)
-    last_power = float(last_power_state.state)
-    #No new energy if last power was zero.
-    if last_power > 0:
-        ##logger.warning("Last Power = {}".format(last_power))
-        last_power_change = last_power_state.last_changed.timestamp()
 
-        ##logger.warning("Power Test = {}".format(power))
-        power_change = power_state.last_changed.timestamp()
-        ##logger.warning("power_change = {}".format(power_change))
+    ##logger.warning("Power Test = {}".format(power))
+    power_change = power_state.last_changed.timestamp()
+    ##logger.warning("power_change = {}".format(power_change))
 
-        delta_time = power_change - last_power_change
-        #logger.warning("delta_time = {}".format(delta_time))
+    delta_time = power_change - last_power_change
+    #logger.warning("delta_time = {}".format(delta_time))
 
-        #Convert to energy per hour
-        delta_energy = delta_time * last_power / WINDOW_SIZE
-
-        ##logger.warning("energy_accum = {}".format(energy_accum))
-        #logger.warning("delta_energy = {}".format(delta_energy))
-        energy_accum = round(energy_accum + delta_energy,1)
-        hass.states.set(input_number_energy_acum, energy_accum, {"unit_of_measurement": energy_unit})
-        #logger.warning("energy_accum next = {}".format(energy_accum))
+    #Convert to energy per hour
+    delta_energy = delta_time * last_power / WINDOW_SIZE
+    #logger.warning("delta_energy = {}".format(delta_energy))
     
-    #Update last_power with new power
-    hass.states.set(input_number_last_power, power, {"unit_of_measurement": power_unit})
+    energy_accum = round(energy_accum + delta_energy,1)
+    #logger.warning("energy_accum = {}".format(energy_accum))
+    hass.states.set(input_number_energy_acum, energy_accum, {"unit_of_measurement": energy_unit})
+    #logger.warning("energy_accum next = {}".format(energy_accum))
+
+#Update last_power with new power
+hass.states.set(input_number_last_power, power, {"unit_of_measurement": power_unit})
 
 
---
+++ python_scripts/energy_hour.py
@@ -0,0 +1,157 @@
+
+'''
+Date:  November 5, 2020
+
+Versions used: HA116.1, HassOS and Raspberry PI 3 B
+
+Description:
+This Python Script calculates the energy used from the last power change to the hour boundary
+time, adds the energy accumulation over the last hour.  The hourly energy is recorded in 
+"hourly_energy" and the energy accumulation is reset for the next hour window.
+This script waits for the energy script calculation to finish in the case where there is
+a power level change at the hour window boundary.
+It needs three input_number entities to be passed as data values:  power, last_power and hourly_energy
+input_number.energy_accum is used to accumulate the energy calculation over an hoour period.
+input_number entities restore the last known value on HA restart.  Sensors do not restore.
+
+Instructions:
+1. Python Script setup
+   1.1 Create /config/python_scripts directory and copy this file to it.
+   1.2 Add this line to configuration.yaml
+       python_scripts:    
+        
+2.  Add these input_number entity to configuration.yaml
+    input_number used because their values are restored after HA restarts.
+    
+    furnace_power:
+      name: Furnace Power
+      min: 0
+      max: 50
+      step: 0.1
+      unit_of_measurement: 'kW'
+    
+    last_power:
+      min: 0
+      max: 100
+      step: 0.1
+      mode: box
+      
+    energy_accum:
+      min: 0
+      max: 1000
+      step: 0.1 
+      
+    hourly_energy:
+      min: 0
+      max: 100
+      step: 0.1
+      mode: box 
+        
+3.  Add this automation to automations.yaml
+    
+
+- alias: Hourly Energy
+  initial_state: True
+  trigger:
+    - platform: time_pattern
+      hours: "/1"
+  action:
+    service: script.turn_on
+    entity_id: script.energy_hour
+    
+4. Add this script to scripts.yaml
+* items are your choosing but must match your sensor names above
+    input_number.furnace_power is the input power sensor in this example
+
+energy_hour:
+  alias: Hourly Energy
+  sequence:
+  #delay start if furnace power is changing of hour boundary.
+    - wait_template: "{{ is_state('script.energy', 'off') }}"
+    - service: python_script.energy_hour
+      data:
+        power: furnace_power*
+        last_power: last_power
+        energy_accum: energy_accum
+        hourly_energy: hourly_energy  
+
+
+5. Use the history_graph lovelace card to display the result.
+
+6. Save database size by not recording the last power and energy accumulator values.  
+   Only the last value needed by the algorithm to calculate the hourly energy.
+   The last value of all sensors is recorded in the database.
+
+7. A separate automation "Daily Energy Total" and Python Script, "hour2day.py" is used 
+   to accumulate hourly energy values on the half hour and then to transfer the daily
+   accumulation to "input_number.daily_energy" at Midnight.
+   There should be a correlation between the daily average temperature difference
+   and the daily average energy consumption.
+
+'''
+
+#logger.warning("Got to energy_hour")
+WINDOW_SIZE = 60*60
+
+power = data.get('power', '0')
+last_power = data.get('last_power', '0')
+energy_accum = data.get('energy_accum', '0')
+hourly_energy = data.get('hourly_energy', '0')
+
+if power != '0':
+	input_number_power = "input_number." + power
+else:
+	logger.error("Energy script missing power data.")
+    
+if last_power != '0':
+	input_number_last_power = "input_number." + last_power
+else:
+	logger.error("Energy script missing last_power data.")
+    
+if energy_accum != '0':
+	input_number_energy_acum = "input_number." + energy_accum
+else:
+	logger.error("Energy script missing energy_accum data.")
+
+if hourly_energy != '0':
+	input_number_hourly_energy = "input_number." + hourly_energy
+else:
+	logger.error("Energy script missing hourly_energy data.")
+
+# timestamp is required to calculate energy from last power to the end of the window
+time = datetime.datetime.now()
+timestamp = time.timestamp()
+
+# Get power unit and use to set the units for the other sensors used.
+power_attr = hass.states.get(input_number_power).attributes
+power_unit = power_attr['unit_of_measurement']
+energy_unit = power_unit + 'h'
+    
+energy_accum_state = hass.states.get(input_number_energy_acum)
+energy_accum = float(energy_accum_state.state)
+
+#Clear energy accumulation at start of interval.
+#Transfer sum of energy from previous period to the hourly log
+
+#logger.warning("Start of Window")
+last_power_state = hass.states.get(input_number_last_power)
+last_power = float(last_power_state.state)
+##logger.warning("Last Power = {}".format(last_power))
+last_power_change = last_power_state.last_changed.timestamp()
+delta_time = timestamp - last_power_change
+##logger.warning("Time since last power change = {}".format(delta_time))    
+#Add remnant from last window
+if last_power > 0:
+    remnant = last_power * delta_time / WINDOW_SIZE
+    energy_accum = energy_accum + remnant
+    #refresh last_power to change the timestamp to beginning of window period
+    hass.states.set(input_number_last_power, (last_power + 0.1), {"unit_of_measurement": power_unit})
+    hass.states.set(input_number_last_power, last_power, {"unit_of_measurement": power_unit})
+#Update hourly energy
+hass.states.set(input_number_hourly_energy, round(energy_accum, 1), {"unit_of_measurement": energy_unit})        
+# Clear energy accumulation for the start of the next window
+hass.states.set(input_number_energy_acum, 0, {"unit_of_measurement": energy_unit})
+#logger.warning("Last Window energy_accum = {}".format(energy_accum))    
+
+
+
