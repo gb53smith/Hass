a0531f0f11c3b9021009fb0afb3b173bf2e1df8f
Graham Smith <gb53smith@gmail.com>
2020-12-07 13:10:08 -0800

    Heat Pump Automation added and getrain.py fixed.

--- getrain.py
+++ getrain.py
@@ -1,34 +1,27 @@
 '''
-Reads weather report from gc.ca website in xml format.  Extracts
-and returns yesterday's rainfall.
+Reads weather report from weather.gc.ca website in html format.  
+Extracts and returns yesterday's rainfall.
+It relies on a particular HTML format so it can break.
 '''
-import xml.etree.ElementTree as ET
+import re
 from urllib import request
 
 # Now I can make the API call.
 id_request = request.urlopen\
-    ('http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/BC/s0000141_e.xml')
+    ('https://weather.gc.ca/city/pages/bc-74_metric_e.html')
 
-#Let's now read this baby in XML format!
-id_pubmed = id_request.read()
-root = ET.fromstring(id_pubmed)
+html_data = id_request.read().decode('utf-8')  # Need to convert bytes to string
 
-# Find the data I want
-# elements are numbered with integers.  Uncomment to see order of elements
-# i = 0
-# for child in root:
-    # print(i,child.tag,child.attrib)
-    # i += 1
-    
-#This is yesterday's rainfall in mm.
-rain = root[8][2].text
-# Report 0.0 if no data found
-if rain == None:
-    print('0.0')
-else:
+# Data is spread over three lines so \n is required.
+m = re.search("(Total Precipitation.*\n.*\n.*millimetres)", html_data)
+
+if m:
+    splitdata = m.group(1).split('">')
+    rain = splitdata[1].split('<')[0].strip()
     # Report 0.1 mm if trace is reported
     if rain[0].isdigit():
         print(rain)
     else:
         print('0.1')
-
+else:
+    print('0.0')
