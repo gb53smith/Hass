35afcf17e1006463e1e5947da49318cec80cc9af
Graham Smith <gb53smith@gmail.com>
2022-03-29 18:44:38 -0700

    Restore deleted files

---
+++ medalert1_check.py
@@ -0,0 +1,29 @@
+'''
+Checks that medalert1 Home Assistant is running.
+Tolerant to 1 error in 3 tries.
+'''
+from urllib.request import urlopen
+from urllib.error import URLError
+count = 0
+try:
+    HTML = urlopen("https://medalert1.duckdns.org:8123", timeout=3)
+    if "Home Assistant" in str(HTML.read()):
+        count += 1
+except URLError:
+    pass
+try:
+    HTML = urlopen("https://medalert1.duckdns.org:8123", timeout=3)
+    if "Home Assistant" in str(HTML.read()):
+        count += 1
+except URLError:
+    pass
+try:
+    HTML = urlopen("https://medalert1.duckdns.org:8123", timeout=3)
+    if "Home Assistant" in str(HTML.read()):
+        count += 1
+except URLError:
+    pass
+if count >= 2 :
+    print("Up" + str(count))
+else:
+    print("FIX")
